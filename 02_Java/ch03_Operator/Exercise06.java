6240ee5de6424ca77ae59a2e1cbe0c796304a0ba
201511094 <kws2205@naver.com>
2020-07-14 15:09:37 +0900

    Add Exercise files in ch03

---
+++ 02_Java/ch03_Operator/Exercise06.java
@@ -0,0 +1,13 @@
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lengthTop=5;
+		int lengthBottom=10;
+		int height=7;
+		double area=(lengthTop+lengthBottom)*height/2.0;	//소수자릿수를 위해
+		
+		System.out.println(area);
+	}
+}
