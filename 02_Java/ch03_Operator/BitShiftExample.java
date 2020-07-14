bc67206a0cd7bd6416f79c036afd0b6f247598d5
201511094 <kws2205@naver.com>
2020-07-14 17:56:34 +0900

    Add Exercise files in ch04

--- 02_Java/ch03_Operator/BitShiftExample.java
+++ 02_Java/ch03_Operator/BitShiftExample.java
@@ -4,8 +4,11 @@ public class BitShiftExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1 << 3 = " + (1<<3));	//1 * 2^3	
-		System.out.println("-8 >> 3 = " + (-8>>3));	//빈공간에 채워넣는 값이 다름
-		System.out.println("-8 >>> 3 = " + (-8>>>3));	//(-8) * 2^3
+		System.out.println("-8 >> 3 = " + (-8>>3));	//빈공간에 채워넣는 값이 1(MSB)
+		System.out.println("-8 >>> 3 = " + (-8>>>3));	//0으로 채워넣기
 
+		System.out.println("3 << 5 = " + (3<<5));		//3 * 2^5
+		System.out.println("-21 >> 4 = " + (-21>>4));	//빈공간에 채워넣는 값이 1(MSB)
+		System.out.println("-21 >>> 4 = " + (-21>>>4));	//0으로 채워넣기
 	}
 }
