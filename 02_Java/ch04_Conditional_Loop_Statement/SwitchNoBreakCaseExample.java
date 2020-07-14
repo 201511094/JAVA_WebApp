844e31493f40478630e1e22c629008eb78d4cfe3
201511094 <kws2205@naver.com>
2020-07-14 15:57:04 +0900

    Create ch04_Conditional_Loop_Statement folder and add conditional-statement example files

---
+++ 02_Java/ch04_Conditional_Loop_Statement/SwitchNoBreakCaseExample.java
@@ -0,0 +1,21 @@
+
+public class SwitchNoBreakCaseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int time = (int)(Math.random()*4) + 8;	//8 ~ 12
+		System.out.println("[현재시간: " + time + " 시]");
+		
+		//break 없는 경우
+		switch(time) {
+			case 8:
+				System.out.println("출근합니다.");
+			case 9:
+				System.out.println("회의를 합니다.");
+			case 10:
+				System.out.println("업무를 봅니다.");
+			default:
+				System.out.println("외근을 나갑니다.");
+		}
+	}
+}
