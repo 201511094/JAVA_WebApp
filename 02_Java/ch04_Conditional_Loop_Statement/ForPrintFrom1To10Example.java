3996ea667541782390dbc9f448f0c3b8e7575b2f
201511094 <kws2205@naver.com>
2020-07-15 16:19:32 +0900

    Modify java file adding code about star problem

--- 02_Java/ch04_Conditional_Loop_Statement/ForPrintFrom1To10Example.java
+++ 02_Java/ch04_Conditional_Loop_Statement/ForPrintFrom1To10Example.java
@@ -3,8 +3,16 @@ public class ForPrintFrom1To10Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1; i<=10; i++) {
-			System.out.println(i);
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(i);
+//		}
+		
+		//대각선으로 출력
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			System.out.println("*");
 		}
 	}
 }
