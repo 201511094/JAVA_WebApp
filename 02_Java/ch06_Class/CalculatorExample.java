fa34dc3d3d107c5cf1ff78c70bfbaf89d58a201d
201511094 <kws2205@naver.com>
2020-07-15 16:07:44 +0900

    Modify Calculator.java adding method calling

--- 02_Java/ch06_Class/CalculatorExample.java
+++ 02_Java/ch06_Class/CalculatorExample.java
@@ -3,17 +3,20 @@ public class CalculatorExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Calculator myCalc = new Calculator();
-		myCalc.powerOn();
+//		Calculator myCalc = new Calculator();
+//		myCalc.powerOn();
+//		
+//		int result1 = myCalc.plus(5, 6);
+//		System.out.println("합한 결과값: " + result1);
+//		
+//		byte x = 10;
+//		byte y = 4;
+//		double result2 = myCalc.divide(x, y);
+//		System.out.println("나눈 결과값: " + result2);
+//		
+//		myCalc.powerOff();
 		
-		int result1 = myCalc.plus(5, 6);
-		System.out.println("합한 결과값: " + result1);
-		
-		byte x = 10;
-		byte y = 4;
-		double result2 = myCalc.divide(x, y);
-		System.out.println("나눈 결과값: " + result2);
-		
-		myCalc.powerOff();
+		Calculator myCalc=new Calculator();
+		myCalc.execute();	//execute메소드 호출
 	}
 }
