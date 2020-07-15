01e8a385c6b528d264e8934433c822c9f968f90c
201511094 <kws2205@naver.com>
2020-07-15 15:09:14 +0900

    Modify java file using nested if-statement

--- 02_Java/ch04_Conditional_Loop_Statement/IfNestedExample.java
+++ 02_Java/ch04_Conditional_Loop_Statement/IfNestedExample.java
@@ -4,7 +4,8 @@ public class IfNestedExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String grade;
-		int score = (int)(Math.random()*20) + 81;	//0+81 ~ 19+81
+		//int score = (int)(Math.random()*20) + 81;	//0+81 ~ 19+81
+		int score = (int)(Math.random()*100) + 1;	//1 ~ 100
 		
 		System.out.println("점수: " + score);
 		
@@ -15,16 +16,33 @@ public class IfNestedExample {
 			else {
 				grade = "A";
 			}
-		}
-		else {	
+		} else if (score >= 80){	
 			if (score >= 85) {
 				grade = "B+";
 			}
 			else {
 				grade = "B";
 			}
+		} else if (score >= 70) {
+			if (score >= 75) {
+				grade = "C+";
+			}
+			else {
+				grade = "C";
+			}
+			
+		} else if (score >= 60) {
+			if (score >= 65) {
+				grade = "D+";
+			}
+			else {
+				grade = "D";
+			}
 		}
-		
+		else {
+			grade="F";
+		}
 		System.out.println("학점: " + grade);
+		
 	}
 }
