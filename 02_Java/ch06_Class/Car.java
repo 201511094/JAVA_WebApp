966230e2e86e1b36bccf8920da607a0e81bc9658
201511094 <kws2205@naver.com>
2020-07-15 15:58:39 +0900

    Modify Car.java adding methods

--- 02_Java/ch06_Class/Car.java
+++ 02_Java/ch06_Class/Car.java
@@ -1,30 +1,74 @@
 
 public class Car {
-	/*
-	String company="현대자동차";
-	String model="그랜저";
-	String color="검정";
-	int maxSpeed=350;
-	int speed;
-	 */
+	
+//	String company="현대자동차";
+//	String model="그랜저";
+//	String color="검정";
+//	int maxSpeed=350;
+//	int speed;
+	 
 	//필드
-	String company="현대자동차";
-	String model;
-	String color;
-	int maxSpeed;
+//	String company="현대자동차";
+//	String model;
+//	String color;
+//	int maxSpeed;
 	
 	//생성자
-	Car() {}
-	Car(String model) {
-		this.model=model;
+//	Car() {}
+//	Car(String model) {
+//		this.model=model;
+//	}
+//	Car(String model, String color) {
+//		this.model=model;
+//		this.color=color;
+//	}
+//	Car(String model, String color, int maxSpeed) {
+//		this.model=model;
+//		this.color=color;
+//		this.maxSpeed=maxSpeed;
+//	}
+	
+	//생성자 중복 지우기
+//	Car() {}
+//	Car(String model){
+//		this(model, "은색", 250);		//맨 밑의 생성자 호출
+//	}
+//	Car(String model, String color) {
+//		this(model, color, 250);	//맨 밑의 생성자 호출
+//	}
+//	Car(String model, String color, int maxSpeed) {
+//		this.model=model;
+//		this.color=color;
+//		this.maxSpeed=maxSpeed;
+//	}
+	
+	int gas;
+	
+	void setGas(int gas) {
+		this.gas=gas;
 	}
-	Car(String model, String color) {
-		this.model=model;
-		this.color=color;
+	
+	boolean isLeftGas() {
+		if (gas == 0) {
+			System.out.println("gas가 없습니다.");
+			return false;
+		}
+		System.out.println("gas가 있습니다.");
+		return true;
+		
 	}
-	Car(String model, String color, int maxSpeed) {
-		this.model=model;
-		this.color=color;
-		this.maxSpeed=maxSpeed;
+	
+	void run() {
+		while(true) {
+			if (gas>0) {
+				System.out.println("달립니다. gas잔량: "+gas);
+				gas -= 1;
+			}
+			else {
+				System.out.println("멈춥니다. gas잔량: "+gas);
+				return;
+			}
+		}
 	}
+	
 }
--- 02_Java/ch06_Class/CarExample.java
+++ 02_Java/ch06_Class/CarExample.java
@@ -18,26 +18,42 @@ public class CarExample {
 		
 		//Car myCar=new Car("검정", 3000);
 		
-		Car car1 = new Car();
-		System.out.println("car1.company : " + car1.company);
-		System.out.println();
+//		Car car1 = new Car();
+//		System.out.println("car1.company : " + car1.company);
+//		System.out.println();
+//		
+//		Car car2 = new Car("자가용");
+//		System.out.println("car2.company : " + car2.company);
+//		System.out.println("car2.model : " + car2.model);
+//		System.out.println();
+//		
+//		Car car3 = new Car("자가용", "빨강");
+//		System.out.println("car3.company : " + car3.company);
+//		System.out.println("car3.model : " + car3.model);
+//		System.out.println("car3.color : " + car3.color);
+//		System.out.println();
+//		
+//		Car car4 = new Car("택시", "검정", 200);
+//		System.out.println("car4.company : " + car4.company);
+//		System.out.println("car4.model : " + car4.model);
+//		System.out.println("car4.color : " + car4.color);
+//		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
 		
-		Car car2 = new Car("자가용");
-		System.out.println("car2.company : " + car2.company);
-		System.out.println("car2.model : " + car2.model);
-		System.out.println();
+		Car myCar = new Car();
+		myCar.setGas(5);	//setGas메소드
+		boolean gasState = myCar.isLeftGas();	//isLeftGas메소드
 		
-		Car car3 = new Car("자가용", "빨강");
-		System.out.println("car3.company : " + car3.company);
-		System.out.println("car3.model : " + car3.model);
-		System.out.println("car3.color : " + car3.color);
-		System.out.println();
+		if (gasState) {
+			System.out.println("출발합니다.");
+			myCar.run();	//run메소드
+		}
 		
-		Car car4 = new Car("택시", "검정", 200);
-		System.out.println("car4.company : " + car4.company);
-		System.out.println("car4.model : " + car4.model);
-		System.out.println("car4.color : " + car4.color);
-		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
+		if (myCar.isLeftGas()) {	//isLeftGas메소드
+			System.out.println("gas를 주입할 필요가 없습니다.");
+		}
+		else {
+			System.out.println("gas를 주입하세요.");
+		}
 		
 	}
 }
