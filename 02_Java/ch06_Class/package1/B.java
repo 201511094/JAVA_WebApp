d9339611437eb41356e371f0b67cb409699b9168
201511094 <kws2205@naver.com>
2020-07-16 11:55:49 +0900

    Modify package's java files

--- 02_Java/ch06_Class/package1/B.java
+++ 02_Java/ch06_Class/package1/B.java
@@ -1,7 +1,18 @@
 package package1;
 
 public class B {
-	A a1=new A(true);
-	A a2=new A(1);
+//	A a1=new A(true);
+//	A a2=new A(1);
 	//A a3=new A("문자열");	//private 접근불가
+	
+	public B() {
+		A a = new A();
+		a.field1=1;
+		a.field2=1;
+		//a.field3=1;	//private 접근불가
+		
+		a.method1();
+		a.method2();
+		//a.method3();	//private 접근불가
+	}
 }
--- 02_Java/ch06_Class/package2/C.java
+++ 02_Java/ch06_Class/package2/C.java
@@ -2,7 +2,18 @@ package package2;
 import package1.A;	//import
 
 public class C {
-	A a1=new A(true);
+//	A a1=new A(true);
 	//A a2=new A(1);	//default 접근불가
 	//A a3=new A("문자열");	//private 접근불가
+	
+	public C() {
+		A a = new A();
+		a.field1=1;
+		//a.field2=1;	//default 접근불가
+		//a.field3=1;	//private 접근불가
+		
+		a.method1();
+		//a.method2();	//default 접근불가
+		//a.method3();	//private 접근불가
+	}
 }
