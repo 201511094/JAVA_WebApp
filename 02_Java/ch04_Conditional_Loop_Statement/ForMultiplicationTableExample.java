59a0bec0285ed3a1ddec85444e5185e05b5be341
201511094 <kws2205@naver.com>
2020-07-16 10:28:48 +0900

    Modify java file adding multiplication code

--- 02_Java/ch04_Conditional_Loop_Statement/ForMultiplicationTableExample.java
+++ 02_Java/ch04_Conditional_Loop_Statement/ForMultiplicationTableExample.java
@@ -3,12 +3,25 @@ public class ForMultiplicationTableExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int m=2; m<=9; m++) {
-			System.out.println("*** 구구단 " + m + "단 ***");
-			
-			for (int n=1; n<=9; n++) {
-				System.out.println(m + " x " + n + " = " + (m*n));
+//		for (int m=2; m<=9; m++) {
+//			System.out.println("*** 구구단 " + m + "단 ***");
+//			
+//			for (int n=1; n<=9; n++) {
+//				System.out.println(m + " x " + n + " = " + (m*n));
+//			}
+//		}
+		
+		//출력포맷
+		for (int i=2; i<10; i++) {
+			System.out.printf("%-15s", "*** "+i+"단 ***");
+		}
+		System.out.println();
+		
+		for (int i=1; i<10; i++) {
+			for (int j=2 ;j<10; j++) {
+				System.out.printf("%-15s", j+" x "+i+" = "+j*i);
 			}
+			System.out.println();
 		}
 	}
 }
