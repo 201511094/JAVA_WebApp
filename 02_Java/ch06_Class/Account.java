9b4ee7eebfdf34393b70a6b37dc2f4ef0dbbe6d8
201511094 <kws2205@naver.com>
2020-07-16 16:08:16 +0900

    Modify Account.java adding annotation

--- 02_Java/ch06_Class/Account.java
+++ 02_Java/ch06_Class/Account.java
@@ -1,26 +1,41 @@
 
 public class Account {
 	
-	public static final int MIN_BALANCE = 0;
-	public static final int MAX_BALANCE = 1000000;
-	
-	int balance;
-
-	public void setBalance(int balance) {
-		// TODO Auto-generated method stub
-		if (balance >= MIN_BALANCE && balance <= MAX_BALANCE) {
-			this.balance = balance;
-		}
-		
-		//또는
-//		if (i < MIN_BALANCE || i > MAX_BALANCE) {
-//			return;
+//	public static final int MIN_BALANCE = 0;
+//	public static final int MAX_BALANCE = 1000000;
+//	int balance;
+//
+//	public void setBalance(int balance) {
+//		// TODO Auto-generated method stub
+//		if (balance >= MIN_BALANCE && balance <= MAX_BALANCE) {
+//			this.balance = balance;
 //		}
-//		this.balance = i;
-	}
+//		//또는
+////		if (i < MIN_BALANCE || i > MAX_BALANCE) {
+////			return;
+////		}
+////		this.balance = balance;
+//	}
+//	public int getBalance() {
+//		// TODO Auto-generated method stub
+//		return balance;
+//	}
 
-	public int getBalance() {
-		// TODO Auto-generated method stub
-		return balance;
+	
+	private String ano;
+	private String owner;
+	private int balance;
+	
+	public Account(String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
 	}
+	
+	public String getAno() { return ano; }
+	public void setAno(String ano) { this.ano = ano; }
+	public String getOwner() { return owner; }
+	public void setOwner(String owner) { this.owner = owner; }
+	public int getBalance() { return balance; }
+	public void setBalance(int balance) { this.balance = balance; }
 }
