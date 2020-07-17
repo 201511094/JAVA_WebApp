d49e0fd06b86ca6d8b7393c5519ad67650bf34e1
201511094 <kws2205@naver.com>
2020-07-17 15:05:18 +0900

    Modify Parent.java and Child.java using casting

--- 02_Java/ch07_Inheritance/Child.java
+++ 02_Java/ch07_Inheritance/Child.java
@@ -1,10 +1,16 @@
 
 public class Child extends Parent {
 	//재정의
-	@Override
-	public void method2() {
-		System.out.println("Child-method2()");	
-	}
+//	@Override
+//	public void method2() {
+//		System.out.println("Child-method2()");	
+//	}
+//	public void method3() {
+//		System.out.println("Child-method3()");	
+//	}
+	
+	public String field2;
+	
 	public void method3() {
 		System.out.println("Child-method3()");	
 	}
--- 02_Java/ch07_Inheritance/ChildExample.java
+++ 02_Java/ch07_Inheritance/ChildExample.java
@@ -3,11 +3,25 @@ public class ChildExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Child child = new Child();
+//		Child child = new Child();
+//		
+//		Parent parent=child;
+//		parent.method1();	//부모클래스의 메소드
+//		parent.method2();	///오버라이딩 -> 자식클래스의 메소드
+//		//parent.method3();	//호출 불가능
 		
-		Parent parent=child;
-		parent.method1();	//부모클래스의 메소드
-		parent.method2();	///오버라이딩 -> 자식클래스의 메소드
-		//parent.method3();	//호출 불가능
+		Parent parent = new Child();	//자동타입변환
+		parent.field1 = "data1";
+		parent.method1();
+		parent.method2();
+		
+		//자식타입에만 선언되어 있으므로 불가능
+//		parent.field2 = "data2";
+//		parent.method3();
+		
+		Child child = (Child) parent;	//강제타입변환
+		child.field2 = "yyy";
+		child.method3();
+		
 	}
 }
