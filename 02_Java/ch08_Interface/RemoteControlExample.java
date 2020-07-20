ca09f1563a9407992235ba834aba78dc369c5a1c
201511094 <kws2205@naver.com>
2020-07-20 10:42:46 +0900

    Modify example file using anonymouse class

--- 02_Java/ch08_Interface/RemoteControlExample.java
+++ 02_Java/ch08_Interface/RemoteControlExample.java
@@ -2,11 +2,17 @@
 public class RemoteControlExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		RemoteControl rc;	//RemoteControl타입 변수 선언
-		//구현 객체 대입
-		//구현 객체를 인터페이스 변수에 대입해서 사용
-		rc = new Television();
-		rc = new Audio();
+		//RemoteControl rc;	//RemoteControl타입 변수 선언
+		
+		//구현 객체 대입, 구현 객체를 인터페이스 변수에 대입해서 사용
+//		rc = new Television();
+//		rc = new Audio();
+		
+		//익명 구현 클래스 - bin\RemoteControlExample$1
+		RemoteControl rc = new RemoteControl() {
+			public void turnOn() {}
+			public void turnOff() {}
+			public void setVolume(int volume) {}
+		};
 	}
 }
