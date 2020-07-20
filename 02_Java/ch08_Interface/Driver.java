5337f0dc287244061a457d56c005351191015296
201511094 <kws2205@naver.com>
2020-07-20 15:00:32 +0900

    Modify Driver.java using instanceof

--- 02_Java/ch08_Interface/Driver.java
+++ 02_Java/ch08_Interface/Driver.java
@@ -2,7 +2,12 @@
 public class Driver {
 	//vehicle은 구현 객체
 	public void drive(Vehicle vehicle) {
-		vehicle.run();	//구현 객체의 run()메소드가 실행됨
+		//vehicle.run();	//구현 객체의 run()메소드가 실행됨
 		
+		if (vehicle instanceof Bus) {
+			Bus bus = (Bus) vehicle;
+			bus.checkFare();	//강제타입변환을 한 이유
+		}
+		vehicle.run();
 	}
 }
