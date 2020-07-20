bfcf5edb02943152ea3b0a831ce2eec78ef96534
201511094 <kws2205@naver.com>
2020-07-20 12:53:00 +0900

    Modify Television.java adding problem code

--- 02_Java/ch06_Class/Television.java
+++ 02_Java/ch06_Class/Television.java
@@ -4,8 +4,34 @@ public class Television {
 	static String model="LCD";
 	static String info;
 	
-	//static block
+	//코드 추가
+	String name;
+	int productYear;
+
+	static String getCompany() {
+		//Television t = new Television();
+		this.name="kjh";	//1 - this키워드는 객체 자신의 참조. static에서 사용 불가
+		//t.name="kjh";
+		return company;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	void setName(String name) {
+		name=name;	//4
+		//this.name=name;
+	}
+	
+	//static block - static초기화를 하는 곳
 	static {
 		info=company+" - "+model;
+		name="kjh";  //2 - name필드는 static이 아니기 때문
+		productYear=2020;	//3 - 2번과 동일
 	}
 }
+
+//static필드는 객체를 생성하지 않고 사용할 수 있는 필드
+//static메소드는 객체를 생성하지 않고 사용할 수 있는 메소드
+//정적멤버 == 클래스멤버 -> true
