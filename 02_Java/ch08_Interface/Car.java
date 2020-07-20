7159605dd8c878dded25b2bf9a7d30ca59fccb18
201511094 <kws2205@naver.com>
2020-07-20 12:00:51 +0900

    Modify Car java files using array

--- 02_Java/ch08_Interface/Car.java
+++ 02_Java/ch08_Interface/Car.java
@@ -1,17 +1,30 @@
 
 public class Car {
 	//인터페이스 타입 필드 선언 및 초기 구현 객체 대입
-	Tire frontLeftTire = new HankookTire();
-	Tire frontRightTire = new HankookTire();
-	Tire backLeftTire = new HankookTire();
-	Tire backRightTire = new HankookTire();
+//	Tire frontLeftTire = new HankookTire();
+//	Tire frontRightTire = new HankookTire();
+//	Tire backLeftTire = new HankookTire();
+//	Tire backRightTire = new HankookTire();
 	
 	//인터페이스에서 설명된 roll() 호출
+//	void run() {
+//		frontLeftTire.roll();
+//		frontRightTire.roll();
+//		backLeftTire.roll();
+//		backRightTire.roll();
+//	}
+	
+	//배열로 변경
+	Tire[] tires = {
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire()
+	};
 	void run() {
-		frontLeftTire.roll();
-		frontRightTire.roll();
-		backLeftTire.roll();
-		backRightTire.roll();
+		for (Tire t : tires) {
+			t.roll();
+		}
 	}
 	
 }
--- 02_Java/ch08_Interface/CarExample.java
+++ 02_Java/ch08_Interface/CarExample.java
@@ -8,8 +8,11 @@ public class CarExample {
 		myCar.run();
 		System.out.println();
 		
-		myCar.frontLeftTire=new KumhoTire();
-		myCar.frontRightTire=new KumhoTire();
+//		myCar.frontLeftTire=new KumhoTire();
+//		myCar.frontRightTire=new KumhoTire();
+		myCar.tires[0]=new KumhoTire();
+		myCar.tires[1]=new KumhoTire();
+		
 		myCar.run();
 		
 	}
