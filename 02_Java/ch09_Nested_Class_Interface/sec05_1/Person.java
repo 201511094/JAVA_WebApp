0f95e2ab01f886d771a4db3d5e863d72595b25bd
201511094 <kws2205@naver.com>
2020-07-21 12:39:43 +0900

    Create sec05 package folders and java files

---
+++ 02_Java/ch09_Nested_Class_Interface/sec05_1/Person.java
@@ -0,0 +1,6 @@
+package sec05_1;
+
+public class Person {
+	void wake() {System.out.println("7시에 일어납니다.");}
+	
+}
---
+++ 02_Java/ch09_Nested_Class_Interface/sec05_2/Button.java
@@ -0,0 +1,18 @@
+package sec05_2;
+
+public class Button {
+	OnClickListener listener;
+	
+	void setOnClickListener(OnClickListener listener) {
+		this.listener = listener;
+	}
+	
+	void touch() {
+		listener.onClick();
+	}
+	
+	interface OnClickListener {
+		void onClick();
+	}
+	
+}
