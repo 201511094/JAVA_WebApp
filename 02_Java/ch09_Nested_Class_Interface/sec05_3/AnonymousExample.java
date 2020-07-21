f30ef5059c74989e097fc00b09806edf3766966f
201511094 <kws2205@naver.com>
2020-07-21 12:52:18 +0900

    Create sec05 package folder and add java files about local variable access

---
+++ 02_Java/ch09_Nested_Class_Interface/sec05_3/AnonymousExample.java
@@ -0,0 +1,11 @@
+package sec05_3;
+
+public class AnonymousExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Anonymous anony = new Anonymous();
+		anony.method(0,0);	//field의 값이 10이라 10 출력
+		
+	}
+}
---
+++ 02_Java/ch09_Nested_Class_Interface/sec05_3/Calculatable.java
@@ -0,0 +1,6 @@
+package sec05_3;
+
+public interface Calculatable {
+	public int sum();
+	
+}
