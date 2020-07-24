e10d105f749798e19bc5b1b11c8a879d08203cf0
201511094 <kws2205@naver.com>
2020-07-24 18:15:41 +0900

    Add java files about try-with-resources and AutoCloseable interface

---
+++ 02_Java/ch10_Exception/FileInputStream.java
@@ -0,0 +1,20 @@
+
+public class FileInputStream implements AutoCloseable {
+	private String file;
+
+	public FileInputStream(String file) {
+		this.file=file;
+	}
+	
+	public void read() {
+		System.out.println(file+"을 읽습니다.");
+	}
+	
+	//예외가 발생해도 자동으로 닫힘
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(file+"을 닫습니다.");
+	}
+	
+} 
