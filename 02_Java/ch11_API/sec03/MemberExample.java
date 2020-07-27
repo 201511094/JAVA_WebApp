a33444a68e35a73aa37963e13cc8533df70dca30
201511094 <kws2205@naver.com>
2020-07-27 14:44:54 +0900

    Modify Member java files using clone

--- 02_Java/ch11_API/sec03/MemberExample.java
+++ 02_Java/ch11_API/sec03/MemberExample.java
@@ -1,5 +1,5 @@
 package sec03;
-
+/*
 public class MemberExample {
 
 	public static void main(String[] args) {
@@ -22,5 +22,32 @@ public class MemberExample {
 		else {
 			System.out.println("obj1과 obj3은 동등하지 않습니다.");
 		}
+	}
+}
+*/
+
+//얕은 복제
+public class MemberExample {
+	public static void main(String[] args) {
+		//원본 객체 생성
+		Member original = new Member("blue", "홍길동", "12345", 25, true);
+		
+		//복제 객체에서 패스워드 변경
+		Member cloned = original.getMember();
+		cloned.password = "67890";
+		
+		System.out.println("[복제 객체의 필드값]");
+		System.out.println("id: " + cloned.id);
+		System.out.println("name: " + cloned.name);
+		System.out.println("password: " + cloned.password);
+		System.out.println("age: " + cloned.age);
+		System.out.println("adult: " + cloned.adult+'\n');
+		
+		System.out.println("[원본 객체의 필드값]");
+		System.out.println("id: " + original.id);
+		System.out.println("name: " + original.name);
+		System.out.println("password: " + original.password);	//원본값은 변함 없음
+		System.out.println("age: " + original.age);
+		System.out.println("adult: " + original.adult);
 	}
 }
