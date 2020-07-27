b7b70b701139ae97914f13059ee8f3657e451ead
201511094 <kws2205@naver.com>
2020-07-27 15:03:15 +0900

    Add Counter java files about finalize

---
+++ 02_Java/ch11_API/sec03/FinalizeExample.java
@@ -0,0 +1,15 @@
+package sec03;
+
+public class FinalizeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Counter counter = null;
+		for (int i=0; i<50; i++) {
+			counter = new Counter(i);
+			
+			counter = null;
+			System.gc();//Garbage Collector 실행 요청
+		}
+	}
+}
