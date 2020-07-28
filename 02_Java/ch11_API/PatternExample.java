6dfc411ea5d3d789603509a0e7a9ac4e5737a563
201511094 <kws2205@naver.com>
2020-07-28 15:20:09 +0900

    Add java file about Pattern class

---
+++ 02_Java/ch11_API/PatternExample.java
@@ -0,0 +1,29 @@
+import java.util.regex.Pattern;
+
+public class PatternExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String regExp = "(02|010)-\\d{3,4}-\\d{4}";
+		String data = "010-123-4567";
+		boolean result = Pattern.matches(regExp, data);
+		
+		System.out.print("전화번호 검증: ");
+		if (result == true) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+		
+		regExp = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+		data = "angel@navercom";
+		result = Pattern.matches(regExp, data);
+		
+		System.out.print("이메일 검증: ");
+		if (result == true) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+	}
+}
