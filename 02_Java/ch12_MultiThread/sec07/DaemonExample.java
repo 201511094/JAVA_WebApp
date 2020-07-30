d7d10b4e5dd5a443754e7a66249c5bb5cc8d76c4
201511094 <kws2205@naver.com>
2020-07-30 17:46:10 +0900

    Add package folder and files about deamon thread

---
+++ 02_Java/ch12_MultiThread/sec07/DaemonExample.java
@@ -0,0 +1,16 @@
+package sec07;
+
+public class DaemonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AutoSaveThread autoSaveThread=new AutoSaveThread();
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		try {Thread.sleep(3000);}
+		catch (InterruptedException e) {}
+		System.out.println("메인스레드 종료");
+		
+	}
+}
