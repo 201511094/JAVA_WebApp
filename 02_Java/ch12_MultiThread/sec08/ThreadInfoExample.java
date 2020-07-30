714fa99c0395ccbe90dfbd6956968152847fff8c
201511094 <kws2205@naver.com>
2020-07-30 17:46:36 +0900

    Add package folder and files about thread group

---
+++ 02_Java/ch12_MultiThread/sec08/ThreadInfoExample.java
@@ -0,0 +1,26 @@
+package sec08;
+
+import java.util.Map;
+import java.util.Set;
+
+import sec07.AutoSaveThread;
+
+public class ThreadInfoExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AutoSaveThread autoSaveThread=new AutoSaveThread();
+		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		Set<Thread> threads = map.keySet();
+		for (Thread thread: threads) {
+			System.out.println("Name: " + thread.getName() + ((thread.isDaemon())?"(单阁)": "(林)"));
+			System.out.println("家加弊缝: " + thread.getThreadGroup().getName());
+			System.out.println();
+		}
+		
+	}
+}
