86087612f96fff7030bfd402eb57ccba2145b0c3
201511094 <kws2205@naver.com>
2020-08-03 17:52:29 +0900

    Modify example file adding Generic type when calling function

--- 02_Java/ch13_Generic/sec05/BoundedTypeParameterExample.java
+++ 02_Java/ch13_Generic/sec05/BoundedTypeParameterExample.java
@@ -7,10 +7,12 @@ public class BoundedTypeParameterExample {
 		//String str=Util.compare("a", "b");	//String은 Number타입이 아님
 		
 		//타입 파라미터를 명시한 경우
-		int result1=Util.<Integer>compare(10, 20);	//앞이 크면 1, 뒤가 크면 -1
-		System.out.println(result1);
+		int result1=Util.compare(10, 20);	//앞이 크면 1, 뒤가 크면 -1
+		int result2=Util.<Number>compare(4.5, 3);
+		int result3=Util.<Integer>compare((int)4.5, 5);
 		
-		int result2=Util.compare(4.5, 3);
+		System.out.println(result1);
 		System.out.println(result2);
+		System.out.println(result3);
 	}
 }
