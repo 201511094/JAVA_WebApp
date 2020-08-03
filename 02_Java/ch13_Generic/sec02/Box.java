88ad441f2873695e65f42eb816fb8a2e7f977808
201511094 <kws2205@naver.com>
2020-08-03 14:44:34 +0900

    Modify Box java file adding Generic type

--- 02_Java/ch13_Generic/sec02/Box.java
+++ 02_Java/ch13_Generic/sec02/Box.java
@@ -1,13 +1,25 @@
 package sec02;
 
-public class Box {
-	private Object object;
+//public class Box {
+//	private Object object;
+//	
+//	public void set(Object object) {
+//		this.object=object;
+//	}
+//	public Object get() {
+//		return object;
+//	}
+//	
+//}
+
+public class Box<T> {
+	private T t;
 	
-	public void set(Object object) {
-		this.object=object;
+	public void set(T t) {
+		this.t=t;
 	}
-	public Object get() {
-		return object;
+	public T get() {
+		return t;
 	}
 	
 }
--- 02_Java/ch13_Generic/sec02/BoxExample.java
+++ 02_Java/ch13_Generic/sec02/BoxExample.java
@@ -2,13 +2,24 @@ package sec02;
 
 public class BoxExample {
 
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Box box=new Box();
+//		box.set("kjh");	//자동 타입 변환 String->Object
+//		String name=(String)box.get();	//Object to String
+//		
+//		box.set(new Apple());	//자동 타입 변환 Apple->Object
+//		Apple apple=(Apple)box.get();	//Object to Apple
+//	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Box box=new Box();
-		box.set("kjh");	//자동 타입 변환 String->Object
-		String name=(String)box.get();	//Object to String
+		Box<String> box1=new Box<String>();
+		box1.set("hello");
+		String str=box1.get();
 		
-		box.set(new Apple());	//자동 타입 변환 Apple->Object
-		Apple apple=(Apple)box.get();	//Object to Apple
+		Box<Integer> box2=new Box<Integer>();
+		box2.set(6);
+		int value=box2.get();
+		
 	}
 }
