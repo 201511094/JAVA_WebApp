175cb67e89c10308c92ae81901d27864f276c349
201511094 <kws2205@naver.com>
2020-08-03 15:20:59 +0900

    Add package folder and files about Generic method

---
+++ 02_Java/ch13_Generic/sec04/BoxingMethodExample.java
@@ -0,0 +1,14 @@
+package sec04;
+
+public class BoxingMethodExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box<Integer> box1=Util.<Integer>boxing(100);
+		int intValue=box1.get();
+		
+		Box<String> box2=Util.boxing("kjh");
+		String strValue=box2.get();
+		
+	}
+}
