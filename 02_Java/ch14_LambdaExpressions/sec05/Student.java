371d367f0e5bd465ffb7902aa07ceb3418b31126
201511094 <kws2205@naver.com>
2020-08-04 15:32:34 +0900

    Add java file and modify Student.java

--- 02_Java/ch14_LambdaExpressions/sec05/Student.java
+++ 02_Java/ch14_LambdaExpressions/sec05/Student.java
@@ -1,18 +1,31 @@
 package sec05;
 
 public class Student {
-	private String name;
-	private int englishScore;
-	private int mathScore;
+//	private String name;
+//	private int englishScore;
+//	private int mathScore;
+//	
+//	public Student(String name, int englishScore, int mathScore) {
+//		this.name = name;
+//		this.englishScore = englishScore;
+//		this.mathScore = mathScore;
+//	}
+//
+//	public String getName() {return name;}
+//	public int getEnglishScore() {return englishScore;}
+//	public int getMathScore() {return mathScore;}
 	
-	public Student(String name, int englishScore, int mathScore) {
+	private String name;
+	private String sex;
+	private int score;
+	
+	public Student(String name, String sex, int score) {
 		this.name = name;
-		this.englishScore = englishScore;
-		this.mathScore = mathScore;
+		this.sex = sex;
+		this.score = score;
 	}
 
-	public String getName() {return name;}
-	public int getEnglishScore() {return englishScore;}
-	public int getMathScore() {return mathScore;}
+	public String getSex() {return sex;}
+	public int getScore() {return score;}
 	
 }
